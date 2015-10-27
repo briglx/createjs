3106b112bdca81659c2f304359324d3667269097
Brig Lamoreaux <brig.lamoreaux@apollo.edu>
2015-10-27 12:01:48 -0700

    Add dynamic movement

--- js/main.js
+++ js/main.js
@@ -1,61 +1,72 @@
- var stage = new createjs.Stage("stage");
- var image = new createjs.Bitmap("images/badguy.png");
+var stage = new createjs.Stage("stage");
  
- image.age = "45";
- image.width = 20;
-
- var i = 0;
-
- stage.addChild(image);
+var i = 0;
 
 var badguys = [];
-badguys.push(image);
 
- createjs.Ticker.addEventListener("tick", handleTick);
- function handleTick(event) {
+createjs.Ticker.addEventListener("tick", handleTick);
+function handleTick(event) {
 
-    if(i % 50 == 0){
-        var badguy = new createjs.Bitmap("images/badguy.png");
-        badguy.x = getRandomInt(0,300)
-        badguy.scaleX = 0.25;
-        badguy.scaleY = 0.25;
+    if(i % 30 == 0){
+
+        var badguy = new createjs.Bitmap("images/badguy2.png");
+        badguy.wait = getRandomInt(10,20);
+        badguy.wait = 0;
+        badguy.vector = {x: getRandomInt(-10,10), y: getRandomInt(0,2)};
+        badguy.vector = {x: 0, y: 1};
+        badguy.x = getRandomInt(0,200);
+        badguy.scaleX = 0.07;
+        badguy.scaleY = 0.07;
         badguys.push(badguy);
 
         stage.addChild(badguy);
          
     }
+
     i++;
 
     // Update each badguy
+    var remove = [];
     for(var n=0; n<badguys.length; n++){
 
         bg = badguys[n];
 
-        if(bg.x < (stage.canvas.clientWidth - 600)){
-            bg.x += getRandomInt(-1,1);
-            
-        } else {
-            bg.x += getRandomInt(-1,0);
+        if(bg.wait && bg.wait > 0){
+            bg.x += bg.vector.x;    
+            bg.wait--;
+        }
+        else {
+            // Get new vector
+            bg.wait = getRandomInt(5,10);
+            bg.vector = {x: getRandomInt(-10,10), y: getRandomInt(0,2)};
+            bg.vector = {x: 0, y: 1};
+        }
+        
+        bg.y += bg.vector.y;
+
+        if(bg.y > 200){
+            remove.push(n);
         }
 
-
-        bg.y += 1;
+        bg.scaleX = bg.scaleX * (1 + bg.y / 10000);
+        console.log(bg.scaleX * (1 + bg.y / 10000));
+        bg.scaleY = bg.scaleX 
+           
     }
 
-    stage.update();
+    for(var n=0; n<remove.length; n++){
+        badguys.splice(n, 1);
+    }
 
-    
+    console.log(badguys.length);
 
-    
- }
+    stage.update();   
 
+}
 
-stage.addEventListener("click", function(event) {
-     
-
-
+stage.addEventListener("click", function(event) { 
+    console.log('hi');
 });
-
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
